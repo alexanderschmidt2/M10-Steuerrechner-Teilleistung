330db23b1f340009e9ed29b2b18c2f63bc8fc085
alexanderschmidt2 <67632384+alexanderschmidt2@users.noreply.github.com>
2021-05-19 18:31:52 +0200

    erste Abfrage
    Aus einer DB kann dynamisch eine dropdown liste generiert werden

--- js/client_logik.js
+++ js/client_logik.js
@@ -1,12 +1,22 @@
 window.addEventListener("load", init);
 
-function init() {
-let xhr_laenderdaten = new XMLHttpRequest();
-xhr_laenderdaten.open("GET", "127.0.0.1:5500/json/abgabedaten.json")
-xhr_laenderdaten.send();
-xhr_laenderdaten.addEventListener("load", function(){laenderdaten_parsen(xhr_laenderdaten);});    
-var laenderdaten = function laenderdaten_parsen(xhr_laenderdaten){
-    return 
-}
+function init() { //TODO@ Simon Schade, bitte den Code schöner machen und SW fragen
+    let xhr_laenderdaten = new XMLHttpRequest();
+    xhr_laenderdaten.open("GET", "http://127.0.0.1:5500/json/laenderdaten.json");
+    xhr_laenderdaten.send();
+    xhr_laenderdaten.addEventListener("load", function() { parsen(xhr_laenderdaten); })
 };
-function laenderoption() { };
+
+function parsen(xhr_laenderdaten) {
+    var laenderdaten = JSON.parse(xhr_laenderdaten.responseText);
+    laenderoption(laenderdaten);
+};
+
+function laenderoption(laenderdaten) {
+    console.log(laenderdaten);
+    for (let i = 0; i <= laenderdaten.laender.length; i++) {
+        let option = document.createElement("option");
+        option.innerHTML = laenderdaten.laender[i].name;
+        document.getElementById("land").appendChild(option);
+    }
+};
