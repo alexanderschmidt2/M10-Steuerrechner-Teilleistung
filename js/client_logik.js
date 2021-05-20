46db0b0178a621da18ff3fb4e897845f8a227055
alexanderschmidt2 <67632384+alexanderschmidt2@users.noreply.github.com>
2021-05-20 09:29:06 +0200

    Update client_logik.js

--- js/client_logik.js
+++ js/client_logik.js
@@ -1,22 +1,20 @@
 window.addEventListener("load", init);
 
+
 function init() { //TODO@ Simon Schade, bitte den Code schöner machen und SW fragen
     let xhr_laenderdaten = new XMLHttpRequest();
     xhr_laenderdaten.open("GET", "http://127.0.0.1:5500/json/laenderdaten.json");
     xhr_laenderdaten.send();
-    xhr_laenderdaten.addEventListener("load", function() { parsen(xhr_laenderdaten); })
+    xhr_laenderdaten.addEventListener("load", function() { laenderoption(xhr_laenderdaten); })
 };
 
-function parsen(xhr_laenderdaten) {
-    var laenderdaten = JSON.parse(xhr_laenderdaten.responseText);
-    laenderoption(laenderdaten);
-};
-
-function laenderoption(laenderdaten) {
-    console.log(laenderdaten);
-    for (let i = 0; i <= laenderdaten.laender.length; i++) {
-        let option = document.createElement("option");
-        option.innerHTML = laenderdaten.laender[i].name;
-        document.getElementById("land").appendChild(option);
+function laenderoption(xhr_laenderdaten) {
+    let slaenderdaten = JSON.parse(xhr_laenderdaten.responseText);
+    if (laenderdaten != null) {
+        for (let i = 0; i <= laenderdaten.laender.length; i++) {
+            let option = document.createElement("option");
+            option.innerHTML = laenderdaten.laender[i].name;
+            document.getElementById("land").appendChild(option);
+        }
     }
 };
