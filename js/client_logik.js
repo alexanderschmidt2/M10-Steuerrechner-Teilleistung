3041d0fd98df973e5a1facc4231e26e4e7fd6f4e
alexanderschmidt2 <67632384+alexanderschmidt2@users.noreply.github.com>
2021-05-21 11:12:49 +0200

    arbeitsstand @SimonSchade

--- js/client_logik.js
+++ js/client_logik.js
@@ -10,8 +10,8 @@ function init() { //TODO@ Simon Schade, bitte den Code schöner machen und SW fr
     let xhr_abgabedaten = new XMLHttpRequest();
     xhr_abgabedaten.open("GET", "http://127.0.0.1:5500/json/abgabedaten.json");
     xhr_abgabedaten.send();
-    xhr_abgabedaten.addEventListener("load", function () { abgabepruefung(xhr_abgabedaten, xhr_laenderdaten) });
-
+    xhr_abgabedaten.addEventListener("load", function () { abgabepruefung(xhr_abgabedaten) });
+    //document.getElementsById("abgaben").addEventListener("change", warenpruefung());
     function laenderoption(xhr_laenderdaten) {
         let laenderdaten = JSON.parse(xhr_laenderdaten.responseText);
         if (laenderdaten != null) {
@@ -25,59 +25,65 @@ function init() { //TODO@ Simon Schade, bitte den Code schöner machen und SW fr
 
         }
     };
-    function muellsammler(id) {
+
+
+    function muellsammler(id) {//TODO: Schmittwilken fragen, wie wir das selektieren müssen
         if (document.getElementById(id) != null) {
             document.getElementById(id).remove();
         }
     };
 
     //code vereinheitlichen
-    function abgabepruefung(xhr_abgabedaten, xhr_laenderdaten) {
+    function abgabepruefung(xhr_abgabedaten) {
         let abgabedaten = JSON.parse(xhr_abgabedaten.responseText);
-        let laenderdaten = JSON.parse(xhr_laenderdaten.responseText);
         let land = document.getElementById("land");
         land.addEventListener("change", function () {
             let laenderdaten_ausgewaehlt = JSON.parse(land.value);
             console.log(laenderdaten_ausgewaehlt.einfuhr);
             if (laenderdaten_ausgewaehlt.einfuhr == 0) {
-                muellsammler("box");
+                muellsammler("box_einfuhr_land");
                 let einfuhr_box = document.createElement("div");
                 let einfuhr_box_p = document.createElement("p");
-                einfuhr_box.id = "box";
+                einfuhr_box.id = "box_einfuhr_land";
+                einfuhr_box.classList.add("einfuhr_verboten");
                 einfuhr_box_p.innerHTML = laenderdaten_ausgewaehlt.beschreibung;
                 einfuhr_box.appendChild(einfuhr_box_p);
                 document.getElementById("abgabenrechner").appendChild(einfuhr_box);
             } else if (laenderdaten_ausgewaehlt.einfuhr == 1) {
-                abgabeoptionen(abgabedaten, laenderdaten);
+                abgabeoptionen(abgabedaten);
             }
         }
         )
 
     };
-    function abgabeoptionen(abgabedaten, laenderdaten) {
+    function abgabeoptionen(abgabedaten) {
         if (abgabedaten != null) {
-            muellsammler("box");
+            muellsammler("box_einfuhr_land");
             let box_abgaben = document.createElement("div");
-            box_abgaben.id = "box";
+            box_abgaben.id = "box_einfuhr_land";
+            box_abgaben.classList.add("einfuhr_erlaubt");
             let array = [document.createElement("label"), document.createElement("select")];
             array[0].htmlFor = "abgaben";
             array[0].innerHTML = "Warenart";
             array[1].id = "abgaben";
-            for (let i = 0; i <= array.length; i++) {
+            for (let i = 0; i < array.length; i++) {
                 console.log(array[i]);
                 if (array[i] != undefined) {
                     box_abgaben.appendChild(array[i])
                 };
-            document.getElementById("abgabenrechner").appendChild(box_abgaben);    
+                document.getElementById("abgabenrechner").appendChild(box_abgaben);
             };
             for (let k = 0; k <= abgabedaten.produktart.length; k++) {
                 let option_produkte = document.createElement("option");
+                option_produkte.value = JSON.stringify(abgabedaten.produktart[k]);
                 option_produkte.classList.add("option_produkte");
-                
                 option_produkte.innerHTML = abgabedaten.produktart[k].name;
                 document.getElementById("abgaben").appendChild(option_produkte);
             }
 
         }
     }
+    //function warenpruefung();
+    //muellsammler("box_einfuhr_waren");
+    //label, input
 };
