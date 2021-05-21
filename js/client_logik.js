76e17c04d1bbc17c0cf1eb629842c2b22b4b9996
alexanderschmidt2 <67632384+alexanderschmidt2@users.noreply.github.com>
2021-05-21 10:24:50 +0200

    2 boxen erscheinen dynamisch

--- js/client_logik.js
+++ js/client_logik.js
@@ -25,18 +25,40 @@ function init() { //TODO@ Simon Schade, bitte den Code schöner machen und SW fr
 
         }
     };
+    function muellsammler(id) {
+        if (document.getElementById(id) != null) {
+            document.getElementById(id).remove();
+        }
+    };
+
+    //code vereinheitlichen
     function abgabepruefung(xhr_abgabedaten, xhr_laenderdaten) {
         let abgabedaten = JSON.parse(xhr_abgabedaten.responseText);
         let laenderdaten = JSON.parse(xhr_laenderdaten.responseText);
-        let land = document.getElementsByClassName("land");
+        let land = document.getElementById("land");
         land.addEventListener("change", function () {
-            console.log(land.value);
-        })
+            let laenderdaten_ausgewaehlt = JSON.parse(land.value);
+            console.log(laenderdaten_ausgewaehlt.einfuhr);
+            if (laenderdaten_ausgewaehlt.einfuhr == 0) {
+                muellsammler("box");
+                let einfuhr_box = document.createElement("div");
+                let einfuhr_box_p = document.createElement("p");
+                einfuhr_box.id = "box";
+                einfuhr_box_p.innerHTML = laenderdaten_ausgewaehlt.beschreibung;
+                einfuhr_box.appendChild(einfuhr_box_p);
+                document.getElementById("abgabenrechner").appendChild(einfuhr_box);
+            } else if (laenderdaten_ausgewaehlt.einfuhr == 1) {
+                abgabeoptionen(abgabedaten, laenderdaten);
+            }
+        }
+        )
 
     };
-    function abgabeoptionen(abgabedaten) {
-
+    function abgabeoptionen(abgabedaten, laenderdaten) {
         if (abgabedaten != null) {
+            muellsammler("box");
+            let box_abgaben = document.createElement("div");
+            box_abgaben.id = "box";
             let array = [document.createElement("label"), document.createElement("select")];
             array[0].htmlFor = "abgaben";
             array[0].innerHTML = "Warenart";
@@ -44,12 +66,14 @@ function init() { //TODO@ Simon Schade, bitte den Code schöner machen und SW fr
             for (let i = 0; i <= array.length; i++) {
                 console.log(array[i]);
                 if (array[i] != undefined) {
-                    document.getElementById("abgabenrechner").appendChild(array[i])
+                    box_abgaben.appendChild(array[i])
                 };
+            document.getElementById("abgabenrechner").appendChild(box_abgaben);    
             };
             for (let k = 0; k <= abgabedaten.produktart.length; k++) {
                 let option_produkte = document.createElement("option");
                 option_produkte.classList.add("option_produkte");
+                
                 option_produkte.innerHTML = abgabedaten.produktart[k].name;
                 document.getElementById("abgaben").appendChild(option_produkte);
             }
